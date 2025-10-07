1ef509ad67bcb9c93bab121eb5d7c962fe93b8ff
Ioannis Panagi <llelga8@gmail.com>
2025-10-07 11:28:31 +0200

    Project initialization

---
+++ src/main/java/edu/fontysmaua/tournamentapi/TournamentApiApplication.java
@@ -0,0 +1,16 @@
+package edu.fontysmaua.tournamentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class TournamentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/fontysmaua/tournamentapi/TournamentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fontysmaua.tournamentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

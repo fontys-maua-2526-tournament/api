47955467b5dbf3e018cf584dbd93c342c7c8d5b6
Francisco Sanchez <i501613@fontys.student.nl>
2025-10-14 13:08:02 +0200

    modified: build.gradle.kts to include dependencies on msql and flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,10 +28,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-web-services")
+
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
+    implementation("com.mysql:mysql-connector-j")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {

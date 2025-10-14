a0fa3a723ad23bab5caedfd40fa7930f5601b94c
Francisco Sanchez <i501613@fontys.student.nl>
2025-10-14 22:07:44 +0200

    Included Team Repository

---
+++ src/main/java/edu/fontysmaua/tournamentapi/persistence/entity/TeamEntity.java
@@ -0,0 +1,27 @@
+package edu.fontysmaua.tournamentapi.persistence.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "team")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeamEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Length(min = 2, max = 50)
+    @Column(name = "name")
+    private String name;
+}

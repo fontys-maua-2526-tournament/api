28b4be8c9977e03341eea3656fcd9619cdf208c6
Francisco Sanchez <i501613@fontys.student.nl>
2025-10-17 14:09:42 +0200

    Included Tournament Controller class

---
+++ src/main/java/edu/fontysmaua/tournamentapi/controller/TournamentController.java
@@ -0,0 +1,11 @@
+package edu.fontysmaua.tournamentapi.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tournaments")
+@AllArgsConstructor
+public class TournamentController {
+}

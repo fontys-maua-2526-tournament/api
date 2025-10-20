15172316fdb61db4ee157bd396c289150439c021
tokujiTO <tiagomassuda123@gmail.com>
2025-10-20 16:12:58 -0300

    feat: add update Team route

--- src/main/java/edu/fontysmaua/tournamentapi/business/TeamUseCases.java
+++ src/main/java/edu/fontysmaua/tournamentapi/business/TeamUseCases.java
@@ -1,5 +1,8 @@
 package edu.fontysmaua.tournamentapi.business;
+
 import edu.fontysmaua.tournamentapi.domain.Team.GetAllTeamsResponse;
+import edu.fontysmaua.tournamentapi.domain.Team.UpdateTeamResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +12,19 @@ import edu.fontysmaua.tournamentapi.persistence.entity.TeamEntity;
 @Service
 public class TeamUseCases {
 
-    //create - save in the database
-    @Autowired
-    private TeamRepository teamRepository;
-    
-    public void save(TeamEntity team) {
-        this.teamRepository.save(team);
-    }
-    
-    public interface getAllTeams {
-      GetAllTeamsResponse getAllTeams();
-    }
+  // create - save in the database
+  @Autowired
+  private TeamRepository teamRepository;
+
+  public void save(TeamEntity team) {
+    this.teamRepository.save(team);
+  }
+
+  public interface getAllTeams {
+    GetAllTeamsResponse getAllTeams();
+  }
+
+  public interface updateTeam {
+    UpdateTeamResponse updateTeam(TeamEntity team);
+  }
 }
--- src/main/java/edu/fontysmaua/tournamentapi/controller/TeamsController.java
+++ src/main/java/edu/fontysmaua/tournamentapi/controller/TeamsController.java
@@ -2,11 +2,16 @@ package edu.fontysmaua.tournamentapi.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.fontysmaua.tournamentapi.business.TeamUseCases;
 import edu.fontysmaua.tournamentapi.domain.Team.GetAllTeamsResponse;
+import edu.fontysmaua.tournamentapi.domain.Team.UpdateTeamResponse;
+import edu.fontysmaua.tournamentapi.persistence.entity.TeamEntity;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -14,9 +19,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TeamsController {
   private final TeamUseCases.getAllTeams getAllTeamsUseCase;
+  private final TeamUseCases.updateTeam updateTeamUseCase;
 
   @GetMapping
   public ResponseEntity<GetAllTeamsResponse> getAllTeams() {
     return ResponseEntity.ok(getAllTeamsUseCase.getAllTeams());
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<UpdateTeamResponse> updateTeam(@PathVariable Long id, @RequestBody TeamEntity team) {
+    team.setId(id);
+    UpdateTeamResponse response = updateTeamUseCase.updateTeam(team);
+    return ResponseEntity.ok(response);
   }
 }

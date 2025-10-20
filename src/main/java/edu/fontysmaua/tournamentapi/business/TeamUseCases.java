5088fd13a1c4fa7f793e6f3dc9b39dffd8e3cf22
yasmin bonilha <bonilhayaya@gmail.com>
2025-10-20 08:44:13 -0300

    feat: add CreateTeam

--- src/main/java/edu/fontysmaua/tournamentapi/business/TeamUseCases.java
+++ src/main/java/edu/fontysmaua/tournamentapi/business/TeamUseCases.java
@@ -1,5 +1,20 @@
 package edu.fontysmaua.tournamentapi.business;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.fontysmaua.tournamentapi.persistence.TeamRepository;
+import edu.fontysmaua.tournamentapi.persistence.entity.TeamEntity;
+
+@Service
 public class TeamUseCases {
-  
+
+    //create - save in the database
+    @Autowired
+    private TeamRepository teamRepository;
+    
+    public void save(TeamEntity team) {
+        this.teamRepository.save(team);
+    }
+
 }

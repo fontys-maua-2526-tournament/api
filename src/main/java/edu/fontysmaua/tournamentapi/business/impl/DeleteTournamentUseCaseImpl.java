c28cf2bd1a3286ee95cb284fe753e87f576b05e8
Tomás Lopes <i564112@fontysict.nl>
2025-10-21 11:56:38 +0200

    Implemented validation for the Id field

--- src/main/java/edu/fontysmaua/tournamentapi/business/impl/DeleteTournamentUseCaseImpl.java
+++ src/main/java/edu/fontysmaua/tournamentapi/business/impl/DeleteTournamentUseCaseImpl.java
@@ -11,6 +11,12 @@ public class DeleteTournamentUseCaseImpl implements DeleteTournamentUseCase {
     private final TournamentRepository tournamentRepository;
 
     public Long delete(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null.");
+        }
+        if(id <= 0){
+            throw new IllegalArgumentException("ID must be greater than 0.");
+        }
         tournamentRepository.deleteById(id);
         return id;
     }

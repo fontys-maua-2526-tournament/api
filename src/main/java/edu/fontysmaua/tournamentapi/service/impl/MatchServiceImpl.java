80eb6576221be534062f6b7dc0418228174e78a4
Eduardo-Aguiar-LdS <160495392+Eduardo-Aguiar-LdS@users.noreply.github.com>
2025-11-27 10:18:40 -0300

    add: create match at MatchServiceImpl

--- src/main/java/edu/fontysmaua/tournamentapi/service/impl/MatchServiceImpl.java
+++ src/main/java/edu/fontysmaua/tournamentapi/service/impl/MatchServiceImpl.java
@@ -25,4 +25,28 @@ public class MatchServiceImpl implements MatchService {
     public GetAllUpcomingMatchesResponse findAllUpcoming() {
         return new GetAllUpcomingMatchesResponse(matchMapper.entitiesToModels(matchRepository.findAllByTournamentStartTime(LocalDateTime.now())));
     }
+
+    @Override
+    public SavedMatchResponse create(SaveMatchRequest request) {
+        TournamentEntity tournament = tournamentRepository.findById(request.getTournamentId())
+            .orElseThrow(() -> new RuntimeException("Tournament not found"));
+
+        TeamEntity team1 = teamRepository.findById(request.getTeam1Id())
+            .orElseThrow(() -> new RuntimeException("Team 1 not found"));
+
+        TeamEntity team2 = teamRepository.findById(request.getTeam2Id())
+            .orElseThrow(() -> new RuntimeException("Team 2 not found"));
+
+        MatchEntity savedMatch = matchRepository.save(
+            MatchEntity.builder()
+                .tournament(tournament)
+                .round(request.getRound())
+                .team1(team1)
+                .team2(team2)
+                .team1Score(request.getTeam1Score())
+                .team2Score(request.getTeam2Score())
+                .build()
+        );
+        return new SavedMatchResponse(matchMapper.entityToModel(savedMatch));
+    }
 }

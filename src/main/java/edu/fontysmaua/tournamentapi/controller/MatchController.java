bdd78f3b1b4bbe0eeb3f53b579e714d8f40b38e6
Eduardo-Aguiar-LdS <160495392+Eduardo-Aguiar-LdS@users.noreply.github.com>
2025-11-28 09:11:01 -0300

    fix: MatchController imports

--- src/main/java/edu/fontysmaua/tournamentapi/controller/MatchController.java
+++ src/main/java/edu/fontysmaua/tournamentapi/controller/MatchController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/matches")

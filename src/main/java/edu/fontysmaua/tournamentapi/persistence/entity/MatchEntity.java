be94ebd2caf2fdf529ef6c10a05ac106a5256778
Eduardo-Aguiar-LdS <160495392+Eduardo-Aguiar-LdS@users.noreply.github.com>
2025-11-28 09:15:56 -0300

    fix: MatchEntity imports

--- src/main/java/edu/fontysmaua/tournamentapi/persistence/entity/MatchEntity.java
+++ src/main/java/edu/fontysmaua/tournamentapi/persistence/entity/MatchEntity.java
@@ -3,6 +3,9 @@ package edu.fontysmaua.tournamentapi.persistence.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Getter

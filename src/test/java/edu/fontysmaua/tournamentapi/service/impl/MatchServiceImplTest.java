e51fc4cac9ce7a4a6baca7b5c87a4f2da375d1f0
Eduardo-Aguiar-LdS <160495392+Eduardo-Aguiar-LdS@users.noreply.github.com>
2025-12-01 16:40:50 -0300

    fix: MatchServiceImplTest import

--- src/test/java/edu/fontysmaua/tournamentapi/service/impl/MatchServiceImplTest.java
+++ src/test/java/edu/fontysmaua/tournamentapi/service/impl/MatchServiceImplTest.java
@@ -27,6 +27,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;

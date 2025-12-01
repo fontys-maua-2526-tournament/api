1dda14efd17ae8c2626c90a10ad3b7bcd10e0bfa
Eduardo-Aguiar-LdS <160495392+Eduardo-Aguiar-LdS@users.noreply.github.com>
2025-12-01 16:33:38 -0300

    fix: MatchController import

--- src/main/java/edu/fontysmaua/tournamentapi/controller/MatchController.java
+++ src/main/java/edu/fontysmaua/tournamentapi/controller/MatchController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import jakarta.validation.Valid;
 
 @RestController
